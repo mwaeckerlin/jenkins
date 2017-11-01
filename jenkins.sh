8a9a61234900795d35a53f9bf6029378ca662481
Marc Wäckerlin <marc@waeckerlin.org>
2017-11-01 16:14:59 +0100

    variable JAVA is not always set

--- jenkins.sh
+++ jenkins.sh
@@ -4,4 +4,4 @@ export TERMINAL=dumb
 export ANDROID_HOME="${ANDROID_HOME}"
 . /etc/default/jenkins
 export JENKINS_HOME
-${JAVA} -jar ${JAVA_ARGS} -Dfile.encoding=UTF-8 ${JENKINS_WAR} ${JENKINS_ARGS}
+${JAVA:-java} -jar ${JAVA_ARGS} -Dfile.encoding=UTF-8 ${JENKINS_WAR} ${JENKINS_ARGS}
